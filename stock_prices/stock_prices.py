a052d9b6bdf0bd182c49e3ef5cd0272293c09a59
Lexie Jiang <lexiemingjiang@gmail.com>
2020-04-15 15:43:57 -0700

    Commit 1: notes on stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,10 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import argparse
 
 def find_max_profit(prices):
-  pass
+  # We have to make sure that the given list has at least 2 prices to continue
+  if len(prices) < 2:
+    return -1
 
+  # set three variables
+  # set the first index of the first item of the list to the minimum price thus far
+  # if the number after is greater than it becomes the max price so far; if less, then we move on to the next element and compare
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
