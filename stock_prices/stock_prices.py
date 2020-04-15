7f8d75b5fb1caf8c22d064bf2f9db231c31bb93c
Lexie Jiang <lexiemingjiang@gmail.com>
2020-04-15 15:57:50 -0700

    Commit 2: stock_prices.py complete, passed

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -11,6 +11,27 @@ def find_max_profit(prices):
   # set the first index of the first item of the list to the minimum price thus far
   # if the number after is greater than it becomes the max price so far; if less, then we move on to the next element and compare
 
+  max_price_index = 0
+  min_price_now = prices[0]
+  max_price_now = 0
+
+  # finding the ma price
+  # we will start with the 2nd element
+  for i in range(1, len(prices) - 1):
+    if prices[i] > max_price_now:
+      max_price_now = prices[i]
+      max_price_index = i
+  
+  # find min price that exists before max price in the list
+  for i in range(0, max_price_index):
+    if prices[i] < min_price_now:
+      min_price_now = prices[i]
+
+  # return the profit margin (max - min)
+  max_profit = max_price_now - min_price_now
+  
+  return max_profit
+
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
   parser = argparse.ArgumentParser(description='Find max profit from prices.')
